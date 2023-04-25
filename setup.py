1bf97a3c1dbb7336184bbc77ac1cb03541355909
Lollipop69 <carlosantony924@gmail.com>
2023-04-25 13:51:49 +0530

    0.0.3

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     name='discake',
     author='Carlos',
     url='https://github.com/lollipop-69/discake',
-    version='0.0.2',
+    version='0.0.3',
     license='MIT',
     description='A discord py support library containing utilities.',
     long_description=readme,
